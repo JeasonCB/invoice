e6de2ef82c7fc70d58b998b42ef87e37f96a84fd
JeasonCB <88633215+JeasonCB@users.noreply.github.com>
2024-05-01 12:28:21 -0400

    function sum is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+'use client'
+import React, { useState } from 'react';
+import { Input } from '@nextui-org/input';
 import { Button } from '@nextui-org/button';
 
 export default function Home() {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handleButtonClick = () => {
+    let sum = value1 + value2;
+    setResult(sum);
+  }
+
   return (
     <>
-      <Button>Click me</Button>
+      <div className='grid place-items-center h-screen'>
+        <div className='grid w-64 gap-4'>
+          <Input type="number" label="Value1" placeholder='1' onChange={(e) => setValue1(Number(e.target.value))} />
+          <Input type="number" label="Valor2" placeholder='2' onChange={(e) => setValue2(Number(e.target.value))} />
+          <Button size="sm" onClick={handleButtonClick}>Procesar</Button>
+          <p>Resultado: {result}</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
