676a85747576b005311015d6c651827b5b3f09db
JeasonCB <88633215+JeasonCB@users.noreply.github.com>
2024-06-17 23:14:29 -0400

    montaña suites

--- src/app/components/Section.tsx
+++ src/app/components/Section.tsx
@@ -7,7 +7,7 @@ function Section({ title = "Título predeterminado", description = "Descripción
             <div className=" bg-slate-100/75 backdrop:blur-md overflow-hidden">
                 <h1 className="text-4xl ">{title}</h1>
                 <h2 className="text-lg">{description}</h2>
-                <Image src={image} alt="imagen" width={0} height={0} quality={10} sizes="100%" fill objectFit="cover" className="h-auto w-auto absolute inset-0" />
+                <Image priority src={image} alt="imagen" width={0} height={0} quality={10} sizes="100%" fill objectFit="cover" className="h-auto w-auto absolute inset-0" />
                 {/* <Inputs /> */}
             </div>
         </div>
