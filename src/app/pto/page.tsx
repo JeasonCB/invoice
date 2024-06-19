fe357e95a8639c8edec8590f74dc4522feda8321
JeasonCB <88633215+JeasonCB@users.noreply.github.com>
2024-06-19 07:52:26 -0400

    version 2.1

--- src/app/pto/page.tsx
+++ src/app/pto/page.tsx
@@ -4,11 +4,11 @@ function page() {
     return (
         <>
             <section className="flex w-[100dvw] h-[100vh]">
-                <div className=" bg-red-500 w-0 flex-grow object-cover opacity-80 transition-width hover:cursor-crosshair hover:w-[75dvw] hover:opacity-100 hover:contrast-125">
-                    <p className="">Familiar</p>
+                <div className=" bg-green-900 w-0 flex-grow object-cover opacity-80 transition-width hover:cursor-crosshair hover:w-[75dvw] hover:opacity-100 hover:contrast-125">
+                    <p className="break-all text-4xl pr-4 ml-2 uppercase text-white">Familiar</p>
                 </div>
-                <div className=" bg-blue-500 w-0 flex-grow object-cover opacity-80 transition-width  hover:cursor-crosshair hover:w-[75dvw] hover:opacity-100 hover:contrast-125">
-                    <p className="">Express</p>
+                <div className=" bg-black/90 w-0 flex-grow object-cover opacity-80 transition-width  hover:cursor-crosshair hover:w-[75dvw] hover:opacity-100 hover:contrast-125">
+                    <p className="break-all text-4xl  ml-2 uppercase text-white">Express</p>
                 </div>
             </section>
         </>
