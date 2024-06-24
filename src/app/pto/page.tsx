475c01de2f97d226bb5a70d203d838f6a500c2db
JeasonCB <88633215+JeasonCB@users.noreply.github.com>
2024-06-24 19:05:02 -0400

    version 2.2

--- src/app/pto/page.tsx
+++ src/app/pto/page.tsx
@@ -1,18 +1,81 @@
-import Section from "../components/Section";
+'use client'
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+import Head from 'next/head';
 
-function page() {
+const text = "text-white text-4xl pl-2 select-none"
+
+function Page() {
+    const [activeSection, setActiveSection] = useState('familiar');
+    const [isLandscape, setIsLandscape] = useState(window.innerWidth > window.innerHeight);
+
+    // Escuchar cambios en la orientación de la pantalla
+    useEffect(() => {
+        const handleResize = () => {
+            setIsLandscape(window.innerWidth > window.innerHeight);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Limpiar el evento al desmontar el componente
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    // Función para cambiar la sección activa solo si la sección clickeada no está ya activa
+    const toggleSection = (section: string) => {
+        if (activeSection !== section) {
+            setActiveSection(activeSection === 'familiar' ? 'express' : 'familiar');
+        }
+    };
+
+    // Calcular los anchos basados en la orientación y la sección activa
+    const calculateWidths = (section: string) => {
+        if (isLandscape) {
+            return section === activeSection ? '95%' : '5%';
+        } else {
+            return section === activeSection ? '85%' : '15%';
+        }
+    };
+
     return (
         <>
-            <section className="flex w-[100dvw] h-[100vh]">
-                <div className=" bg-green-900 w-0 flex-grow object-cover opacity-80 transition-width hover:cursor-crosshair hover:w-[75dvw] hover:opacity-100 hover:contrast-125">
-                    <p className="break-all text-4xl pr-2 ml-2 uppercase text-white select-none">Familiar</p>
-                </div>
-                <div className=" bg-black/90 w-0 flex-grow object-cover opacity-80 transition-width  hover:cursor-crosshair hover:w-[75dvw] hover:opacity-100 hover:contrast-125">
-                    <p className="break-all text-4xl ml-2 uppercase text-white select-none">Express</p>
-                </div>
-            </section>
+            <Head>
+                <title>Secciones Dinámicas</title>
+            </Head>
+            <div className="flex w-[100%] h-[100vh] overflow-hidden">
+                <motion.div
+                    className={`flex justify-center items-center bg-green-900 ${activeSection === 'familiar' ? 'w-[85%] ' : 'w-[15%]'}`}
+                    animate={{ width: calculateWidths('familiar') }}
+                    transition={{ duration: 0.18 }}
+                    onClick={() => toggleSection('familiar')}
+                >
+                    <motion.h2
+                        className={text}
+                        initial={{ opacity: 1 }}
+                        animate={{ opacity: activeSection === 'familiar' ? 1 : 0 }}
+                        transition={{ duration: 0.1 }}
+                    >
+                        Familiar
+                    </motion.h2>
+                </motion.div>
+                <motion.div
+                    className={`flex justify-center items-center bg-black/80 ${activeSection === 'express' ? 'w-[85%] ' : 'w-[15%]'}`}
+                    animate={{ width: calculateWidths('express') }}
+                    transition={{ duration: 0.18 }}
+                    onClick={() => toggleSection('express')}
+                >
+                    <motion.h2
+                        className={text}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: activeSection === 'express' ? 1 : 0 }}
+                        transition={{ duration: 0.1, delay: 0 }}
+                    >
+                        Express
+                    </motion.h2>
+                </motion.div>
+            </div>
         </>
-    );
+    )
 }
 
-export default page;
+export default Page;
